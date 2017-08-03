670e6cce993c0e2abb98d88382dca33b4a0eaaf9
Gdemirev01 <gdemirev78@gmail.com>
2017-08-03 14:00:09 +0300

    MariaDB settings

--- Testify/settings.py
+++ Testify/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'Testify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Testify',
+	'USER': 'Testifyuser',
+	'PASSWORD': '',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
