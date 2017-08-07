805db8697200cf6d0d923695986e09c691a36785
Gdemirev01 <gdemirev78@gmail.com>
2017-08-07 16:04:52 +0300

    gunicorn stuff

--- Testify/settings.py
+++ Testify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6_)pr(zh11#v@l4tjyr1dw0f5s4yll@ai0afaolxfq707shuh5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
